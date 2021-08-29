44d31252824695eb0bbb14e9debf9284e9ec48e9
Hitbee-dev <ckim.esw@gmail.com>
2021-08-30 05:58:25 +0900

    ResultPage Modification

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -136,9 +136,92 @@ class _MainPageState extends State<MainPage> {
       width: 170,
       child: ElevatedButton(
           onPressed: () {
-            Get.offNamed("/result");
+            if (saved_Q_total.text == "") {
+              SnackPrint("Q: total유량 값을 입력 해 주세요.");
+            } else if (saved_Epsilon.text == "") {
+              SnackPrint("ε: 파이프 조도 값을 입력 해 주세요.");
+            } else if (saved_D_one.text == "") {
+              SnackPrint("D1: 1번 pipe 직경 값을 입력 해 주세요.");
+            } else if (saved_D_two.text == "") {
+              SnackPrint("D2: 2번 pipe 직경 값을 입력 해 주세요.");
+            } else if (saved_Gamma.text == "") {
+              SnackPrint("γw: 물의 비중량 값을 입력 해 주세요.");
+            } else if (saved_Upsilon.text == "") {
+              SnackPrint("υw: 물의 점도 값을 입력 해 주세요.");
+            } else if (saved_L_one.text == "") {
+              SnackPrint("L1: 1번 pipe 길이 값을 입력 해 주세요.");
+            } else if (saved_L_two.text == "") {
+              SnackPrint("L2: 2번 pipe 길이 값을 입력 해 주세요.");
+            } else if (saved_K_one.text == "") {
+              SnackPrint("K1: 1번 pipe 수두손실 계수 값을 입력 해 주세요.");
+            } else if (saved_K_two.text == "") {
+              SnackPrint("K2: 2번 pipe 수두손실 계수 값을 입력 해 주세요.");
+            } else if (saved_F_one.text == "") {
+              SnackPrint("f if1: 1번 pipe 예상 마찰계수 값을 입력 해 주세요.");
+            } else if (saved_F_two.text == "") {
+              SnackPrint("f if2: 2번 pipe 예상 마찰계수 값을 입력 해 주세요.");
+            } else if (saved_Delta.text == "") {
+              SnackPrint("ΔZ: pipe 높이 차 값을 입력 해 주세요.");
+            } else {
+              Get.offNamed("/result",
+                  arguments: SettingData(
+                      saved_Q_total.text,
+                      saved_Epsilon.text,
+                      saved_D_one.text,
+                      saved_D_two.text,
+                      saved_Gamma.text,
+                      saved_Upsilon.text,
+                      saved_L_one.text,
+                      saved_L_two.text,
+                      saved_K_one.text,
+                      saved_K_two.text,
+                      saved_F_one.text,
+                      saved_F_two.text,
+                      saved_Delta.text));
+            }
           },
           child: Text("Result", style: TextStyle(fontWeight: FontWeight.bold))),
     );
   }
+
+  SnackPrint(String message) {
+    scaffoldKey.currentState!.showSnackBar(SnackBar(
+      content: Text(message),
+      action: SnackBarAction(
+        label: 'Done',
+        onPressed: () {},
+      ),
+    ));
+  }
+}
+
+class SettingData {
+  String q_total;
+  String epsilon;
+  String d_one;
+  String d_two;
+  String gamma;
+  String upsilon;
+  String l_one;
+  String l_two;
+  String k_one;
+  String k_two;
+  String f_one;
+  String f_two;
+  String delta;
+
+  SettingData(
+      this.q_total,
+      this.epsilon,
+      this.d_one,
+      this.d_two,
+      this.gamma,
+      this.upsilon,
+      this.l_one,
+      this.l_two,
+      this.k_one,
+      this.k_two,
+      this.f_one,
+      this.f_two,
+      this.delta);
 }
--- lib/page/result_page.dart
+++ lib/page/result_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ResultPage extends StatefulWidget {
   const ResultPage({Key? key}) : super(key: key);
@@ -10,6 +11,46 @@ class ResultPage extends StatefulWidget {
 class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: () =>
+          FocusManager.instance.primaryFocus?.unfocus(), //화면 아무데나 터치해서 내리기
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            "Result",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget resultButton() {
+    return Container(
+      padding: EdgeInsets.only(top: 20),
+      height: 70,
+      width: 170,
+      child: ElevatedButton(
+          onPressed: () {
+            Get.offNamed("/");
+          },
+          child: Text("Back", style: TextStyle(fontWeight: FontWeight.bold))),
+    );
   }
 }
