09914dc008969d97df0e45ce791248b5f8b19751
Hitbee-dev <ckim.esw@gmail.com>
2021-08-30 03:39:06 +0900

    TextEditingController Modification

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -13,13 +13,36 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   late TextEditingController saved_Q_total;
+  late TextEditingController saved_Epsilon;
   late TextEditingController saved_D_one;
+  late TextEditingController saved_D_two;
+  late TextEditingController saved_Gamma;
+  late TextEditingController saved_Upsilon;
+  late TextEditingController saved_L_one;
+  late TextEditingController saved_L_two;
+  late TextEditingController saved_K_one;
+  late TextEditingController saved_K_two;
+  late TextEditingController saved_F_one;
+  late TextEditingController saved_F_two;
+  late TextEditingController saved_Delta;
 
   @override
   void initState() {
     super.initState();
     this.saved_Q_total = TextEditingController();
+    this.saved_Epsilon = TextEditingController();
     this.saved_D_one = TextEditingController();
+    this.saved_D_one = TextEditingController();
+    this.saved_D_two = TextEditingController();
+    this.saved_Gamma = TextEditingController();
+    this.saved_Upsilon = TextEditingController();
+    this.saved_L_one = TextEditingController();
+    this.saved_L_two = TextEditingController();
+    this.saved_K_one = TextEditingController();
+    this.saved_K_two = TextEditingController();
+    this.saved_F_one = TextEditingController();
+    this.saved_F_two = TextEditingController();
+    this.saved_Delta = TextEditingController();
   }
 
   @override
@@ -52,21 +75,21 @@ class _MainPageState extends State<MainPage> {
                     children: [
                       inputData("Q", saved_Q_total, "total유량", "m^3/s"),
                       inputData("D1", saved_D_one, "1번 pipe 직경", "mm"),
-                      inputData("γw", saved_Q_total, "물의 비중량", "N/m^3"),
-                      inputData("L1", saved_D_one, "1번 pipe 길이", "m"),
-                      inputData("K1", saved_Q_total, "1번 pipe 수두손실 계수", ""),
-                      inputData("f if1", saved_D_one, "1번 pipe 예상 마찰계수", ""),
-                      inputData("ΔZ", saved_D_one, "pipe 높이 차", "m"),
+                      inputData("γw", saved_Gamma, "물의 비중량", "N/m^3"),
+                      inputData("L1", saved_L_one, "1번 pipe 길이", "m"),
+                      inputData("K1", saved_K_one, "1번 pipe 수두손실 계수", ""),
+                      inputData("f if1", saved_F_one, "1번 pipe 예상 마찰계수", ""),
+                      inputData("ΔZ", saved_Delta, "pipe 높이 차", "m"),
                     ],
                   ),
                   Column(
                     children: [
-                      inputData("ε", saved_Q_total, "파이프 조도", "mm"),
-                      inputData("D2", saved_D_one, "2번 pipe 직경", "mm"),
-                      inputData("υw", saved_Q_total, "물의 점도", "m^2/s"),
-                      inputData("L2", saved_D_one, "2번 pipe 길이", "m"),
-                      inputData("K2", saved_Q_total, "2번 pipe 수두손실 계수", ""),
-                      inputData("f if2", saved_Q_total, "2번 pipe 예상 마찰계수", ""),
+                      inputData("ε", saved_Epsilon, "파이프 조도", "mm"),
+                      inputData("D2", saved_D_two, "2번 pipe 직경", "mm"),
+                      inputData("υw", saved_Upsilon, "물의 점도", "m^2/s"),
+                      inputData("L2", saved_L_two, "2번 pipe 길이", "m"),
+                      inputData("K2", saved_K_two, "2번 pipe 수두손실 계수", ""),
+                      inputData("f if2", saved_F_two, "2번 pipe 예상 마찰계수", ""),
                       resultButton()
                     ],
                   ),
@@ -112,7 +135,9 @@ class _MainPageState extends State<MainPage> {
       height: 70,
       width: 170,
       child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            Get.offNamed("/result");
+          },
           child: Text("Result", style: TextStyle(fontWeight: FontWeight.bold))),
     );
   }
